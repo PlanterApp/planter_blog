e22601380d4728b6b4f57be48d83a6a77f75e371
Peter Keefe <16809252+percula@users.noreply.github.com>
2023-04-21 11:06:38 -0400

    feat: Remove exif locations

---
+++ netlify/functions/scheduled-deploy.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch'
+import { schedule } from '@netlify/functions'
+
+// This is a sample build hook URL
+const BUILD_HOOK = 'https://api.netlify.com/build_hooks/6442a4587851e905b1620fe6'
+
+// Schedules the handler function to run every hour of every day
+const handler = schedule('0 * * * *', async () => {
+  await fetch(BUILD_HOOK, {
+    method: 'POST'
+  }).then(response => {
+    console.log('Build hook response:', response)
+  })
+
+  return {
+    statusCode: 200
+  }
+})
+
+export { handler }
