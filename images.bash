d666a9d2d2086098b2e22bb9bd43e6e2225df685
Peter Keefe <16809252+percula@users.noreply.github.com>
2024-02-09 10:58:52 -0500

    Update image migration script to work with cover images

--- images.bash
+++ images.bash
@@ -1,13 +1,18 @@
 # shellcheck disable=SC2044
 for i in $(find ./content/posts -type f -name "*.md");
 do
+    echo "--------------------------"
     echo "Markdown file: $i"
     path="${i%/*}/"
     echo "Directory: $path"
+
+    # Look for images in the markdown
     # shellcheck disable=SC2016
     for j in $(rg --pcre2 "\!\[[^\]]*\]\(((?!http).*?)\s*(\"(?:.*[^\"])\")?\s*\)" -or '$1' "$i");
     do
+      echo "--------------------------"
       echo "File: $j"
+      fileName="${j##*/}"
 
       if [[ $j == *"/uploads/"* ]]; then
         echo "Image is in uploads folder"
@@ -26,7 +31,7 @@ do
       echo "UploadCare ID: $id"
 
       if [[ -n "$id" ]]; then
-          uploadCareURL="https://ucarecdn.com/$id/$j"
+          uploadCareURL="https://ucarecdn.com/$id/$fileName"
           echo "UploadCare URL: $uploadCareURL"
           sed -i '' "s+$j+$uploadCareURL+g" "$i"
           rm "$filePath"
@@ -34,6 +39,42 @@ do
       else
           echo "Error migrating $j: UploadCare ID null or empty"
       fi
+
+    done
+
+    # Look for images in the front matter
+    # shellcheck disable=SC2016
+    for j in $(rg --pcre2 "image\s*=\s*\"((?!http).*)\"" -or '$1' "$i");
+        do
+          echo "--------------------------"
+          echo "File: $j"
+          fileName="${j##*/}"
+
+          if [[ $j == *"/uploads/"* ]]; then
+            echo "Image is in uploads folder"
+            filePath="./content$j"
+          else
+            filePath="$path$j"
+          fi
+          echo "FilePath: $filePath"
+
+          json=$(curl -F "UPLOADCARE_PUB_KEY=d92ab83842bf39b45c62" \
+               -F "UPLOADCARE_STORE=auto" \
+               -F "file=@$filePath" \
+               "https://upload.uploadcare.com/base/")
+
+          id=$(echo "$json" | jq -r '.file')
+          echo "UploadCare ID: $id"
+
+          if [[ -n "$id" ]]; then
+              uploadCareURL="https://ucarecdn.com/$id/$fileName"
+              echo "UploadCare URL: $uploadCareURL"
+              sed -i '' "s+$j+$uploadCareURL+g" "$i"
+              rm "$filePath"
+              echo "Successfully migrated $j"
+          else
+              echo "Error migrating $j: UploadCare ID null or empty"
+          fi
 
     done
 done
